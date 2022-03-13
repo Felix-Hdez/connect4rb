9a3fb2849b923211d30d7e7b4fcd9d38e9099b40
FelipeHdez <felipinhdez@gmail.com>
2022-03-13 13:00:55 -0600

    tests for #initializing default pieces and #play barebones

--- connect4.rb
+++ connect4.rb
@@ -1,4 +1,9 @@
 class Connect4
+  attr_accessor :board
+  attr_reader :pieces
+
   def initialize
+    @board = Array.new(6) { Array.new(7) }
+    @pieces = ['●', '●']
   end
 end
--- connect4_spec.rb
+++ connect4_spec.rb
@@ -3,7 +3,7 @@ require_relative 'connect4'
 describe Connect4 do
   subject(:game) { described_class.new }
 
-  describe "initialize" do
+  describe '#initialize' do
     it 'creates new empty board' do
       board = [[nil, nil, nil, nil, nil, nil, nil],
                [nil, nil, nil, nil, nil, nil, nil],
@@ -12,6 +12,30 @@ describe Connect4 do
                [nil, nil, nil, nil, nil, nil, nil],
                [nil, nil, nil, nil, nil, nil, nil]]
       expect(game.board).to eq(board)
+    end
+    context 'when no arguments are passed' do
+      context 'first piece' do
+        it 'is default value' do
+          pieces = game.instance_variable_get(:@pieces)[0]
+          expect(pieces[0]).to eq('●')
+        end
+      end
+      context 'second piece' do
+        it 'is default value' do
+          pieces = game.instance_variable_get(:@pieces)[1]
+          expect(pieces[0]).to eq('●')
+        end
+      end
+    end
+  end
+
+  describe '#play' do
+    before do
+      allow(game).to receive(:print_board)
+    end
+    it 'it calls #print_board' do
+      expect(game).to receive(:print_board)
+      game.play
     end
   end
 end
